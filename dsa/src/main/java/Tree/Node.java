c7a9fc7a299fd27c1c42a582307c50ceb1a9bbd4
Vib-hu <vibhumishra808@gmail.com>
2026-06-26 08:57:13 +0400

    tree changes

--- dsa/src/main/java/Tree/Node.java
+++ dsa/src/main/java/Tree/Node.java
@@ -1,4 +1,17 @@
 package Tree;
 
 public class Node {
+    public Node left;
+    public Node right;
+    public int val;
+
+    public Node(int val, Node left, Node right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    public Node(int val){
+        this.val = val;
+    }
 }
--- dsa/src/main/java/Tree/RecursiveTreeTraversals.java
+++ dsa/src/main/java/Tree/RecursiveTreeTraversals.java
@@ -1,4 +1,43 @@
 package Tree;
 
-public class TreeTraversals {
+public class RecursiveTreeTraversals {
+
+    public void preOrder(Node node){
+        if(node == null)
+            return;
+        System.out.print(node.val);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder(Node node){
+        if(node == null)
+            return;
+        preOrder(node.left);
+        System.out.print(node.val);
+        preOrder(node.right);
+    }
+
+    public void postOrder(Node node){
+        if(node == null)
+            return;
+        preOrder(node.left);
+        preOrder(node.right);
+        System.out.print(node.val);
+    }
+
+    public static void main(String[] args){
+        Node root = new Node(0);
+        root.left = new Node(1);
+        root.right = new Node(2);
+        root.left.left = new Node(3);
+        root.left.right = new Node(4);
+        root.right.left = new Node(5);
+        root.right.right = new Node(6);
+
+        RecursiveTreeTraversals traversals = new RecursiveTreeTraversals();
+        traversals.preOrder(root);
+        traversals.inOrder(root);
+        traversals.postOrder(root);
+    }
 }
